3c5ba943cb0618a604bd7cb6af45ca0a1d4da95b
Tunde Turner <tunde@zerodotdash.com>
2019-02-02 12:07:28 -0600

    Add display messages in chat area

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bill = User.new(username: "bill", password: "password")
+sally = User.new(username: "sally", password: "password")
+tunde = User.new(username: "TUNDE", password: "password")
+
+Message.create(user: bill, body: "Hello everyone!")
+Message.create(user: sally, body: "hey there, bill")
+Message.create(user: tunde, body: "sup!!")
+Message.create(user: bill, body: "I have this really strange thing that happned to me the other day I wanna talk about")
+Message.create(user: tunde, body: "I hope it isn't about that turtle")
+Message.create(user: bill, body: "How did you know!?")
